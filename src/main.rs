ba871ac933c64034155209dd75ad87d454961ac5
junhojava <junho9341@gmail.com>
2024-12-04 18:30:45 +0900

    init rust web server

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use axum::{routing::get, Router};
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+    .route("/api/generate", get(hello_world));
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
